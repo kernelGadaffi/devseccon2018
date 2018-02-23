01008a5812db907f76596a38a6604f8770d0fc20
Wang Hao Lee <wanghao@Wangs-MacBook-Air.local>
2018-02-23 14:09:14 +0800

    adjust username to not use hardcoded password

--- start_app_server.sh
+++ start_app_server.sh
@@ -2,7 +2,7 @@
 # README
 
 # READ SECRET!
-export MONGODB_CREDENTIALS=mongodb://jinkangkor:password@ds147518.mlab.com:47518/kgmyapp
+# export MONGODB_CREDENTIALS=mongodb://jinkangkor:password@ds147518.mlab.com:47518/kgmyapp
 
 # finally, run the app in dev mode...
 npm run dev
